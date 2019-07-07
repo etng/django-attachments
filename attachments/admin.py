37c7943bbe6620d8f08552111e8dd76122e33add
bo.yi <bo.yi@kankanai.com.cn>
2019-07-07 23:56:51 +0800

    [fix]tweak with chinese

--- attachments/admin.py
+++ attachments/admin.py
@@ -3,9 +3,14 @@ from __future__ import unicode_literals
 from django.contrib.contenttypes.admin import GenericStackedInline
 
 from .models import Attachment
+from .forms import AttachmentForm
+from django.utils.translation import ugettext_lazy as _
 
 
 class AttachmentInlines(GenericStackedInline):
     model = Attachment
+    form = AttachmentForm
     exclude = ()
     extra = 1
+    verbose_name = _("Attachment")
+    verbose_name_plural = _("Attachments")
